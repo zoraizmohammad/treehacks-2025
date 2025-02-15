89961dd6e5cd15382870c724bc98c849703ff81d
Mohammad Zoraiz <zoraizmohammad@gmail.com>
2025-02-14 23:17:30 -0800

    adding more index

---
+++ frontend/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SecureForm from './components/SecureForm';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <SecureForm />
+  </React.StrictMode>
+);
