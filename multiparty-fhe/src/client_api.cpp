a37dd1ac3a7f1b67ffc684897449f3cbeca6d31e
Adel Müürsepp <adel.muursepp@gmail.com>
2025-02-15 18:43:00 -0800

    Working but problem decrypting data in server

--- multiparty-fhe/src/client_api.cpp
+++ multiparty-fhe/src/client_api.cpp
@@ -258,6 +258,99 @@ Plaintext vectorToPlaintext(const vector<int>& vec, const CC& cc) {
     return cc->MakePackedPlaintext({sum});
 }
 
+// Add these helper functions at the top of the file, after the includes
+static string base64_encode(const string& input) {
+    static const string base64_chars = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
+    string ret;
+    int i = 0;
+    int j = 0;
+    unsigned char char_array_3[3];
+    unsigned char char_array_4[4];
+    int in_len = input.length();
+    const unsigned char* bytes_to_encode = reinterpret_cast<const unsigned char*>(input.c_str());
+
+    while (in_len--) {
+        char_array_3[i++] = *(bytes_to_encode++);
+        if (i == 3) {
+            char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+            char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+            char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+            char_array_4[3] = char_array_3[2] & 0x3f;
+
+            for(i = 0; i < 4; i++)
+                ret += base64_chars[char_array_4[i]];
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for(j = i; j < 3; j++)
+            char_array_3[j] = '\0';
+
+        char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
+        char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
+        char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
+        char_array_4[3] = char_array_3[2] & 0x3f;
+
+        for (j = 0; j < i + 1; j++)
+            ret += base64_chars[char_array_4[j]];
+
+        while((i++ < 3))
+            ret += '=';
+    }
+    return ret;
+}
+
+static string base64_decode(const string& encoded_string) {
+    static const string base64_chars = 
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz"
+        "0123456789+/";
+
+    string ret;
+    int in_len = encoded_string.size();
+    int i = 0;
+    int j = 0;
+    int in_ = 0;
+    unsigned char char_array_4[4], char_array_3[3];
+
+    while (in_len-- && (encoded_string[in_] != '=') && (isalnum(encoded_string[in_]) || (encoded_string[in_] == '+') || (encoded_string[in_] == '/'))) {
+        char_array_4[i++] = encoded_string[in_]; in_++;
+        if (i == 4) {
+            for (i = 0; i < 4; i++)
+                char_array_4[i] = base64_chars.find(char_array_4[i]);
+
+            char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+            char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+            char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+            for (i = 0; i < 3; i++)
+                ret += char_array_3[i];
+            i = 0;
+        }
+    }
+
+    if (i) {
+        for (j = i; j < 4; j++)
+            char_array_4[j] = 0;
+
+        for (j = 0; j < 4; j++)
+            char_array_4[j] = base64_chars.find(char_array_4[j]);
+
+        char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
+        char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
+        char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+
+        for (j = 0; j < i - 1; j++) ret += char_array_3[j];
+    }
+
+    return ret;
+}
+
 int main() {
     // Create Crow app instance.
     crow::SimpleApp app;
@@ -301,12 +394,14 @@ int main() {
                     // Encrypt the plaintext
                     auto ciphertext = clientInstance.cc->Encrypt(clientInstance.keyPair.publicKey, plaintext);
                     
-                    // Serialize the ciphertext
+                    // Serialize the ciphertext to binary and base64 encode it
                     stringstream ss;
-                    Serial::Serialize(ciphertext, ss, SerType::JSON);
+                    Serial::Serialize(ciphertext, ss, SerType::BINARY);
+                    string binary_str = ss.str();
+                    string base64_str = base64_encode(binary_str);
                     
                     // Add encrypted value to CSV row
-                    csvRow << key << "_encrypted:" << ss.str();
+                    csvRow << base64_str;
                 }
             }
 
